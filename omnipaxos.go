d193933f60c35731af8b6a7a4039ab9967534e8d
aayushhks <aayushhks03@gmail.com>
2025-11-12 22:15:55 -0500

    Heartbeat Timer and Helpers

--- omnipaxos.go
+++ omnipaxos.go
@@ -30,6 +30,14 @@ type OmniPaxos struct {
 	state State
 
 	applyCh chan ApplyMsg
+
+	// Fields added for A4 later
+	missedHbCounts map[int]int
+	restart        Restart
+}
+
+type Restart struct {
+	loop int
 }
 
 type Promise struct {
@@ -151,6 +159,69 @@ func (op *OmniPaxos) killed() bool {
 	return z == 1
 }
 
+func (op *OmniPaxos) max(m map[Ballot]bool) Ballot {
+	res := Ballot{N: -1, Pid: -2}
+	for b := range m {
+		if res.compare(b) < 0 {
+			res = b
+		}
+	}
+	return res
+}
+
+func (op *OmniPaxos) increment(ballot *Ballot, I int) {
+	ballot.N = I + 1
+}
+
+func (op *OmniPaxos) startTimer(delay time.Duration) {
+	for {
+		op.mu.Lock()
+
+		// 1. insert own ballot
+		op.ballots[op.b] = op.qc
+
+		// 2. if have majority, then check leader else qc is false
+		if len(op.ballots) >= (len(op.peers)+1)/2 {
+			// op.checkLeader() // Will be added in next commit
+		} else {
+			op.qc = false
+		}
+
+		// op.updateMissedHbsAndReconnect() // Will be added in A4
+
+		// 3. clear ballot and increase the round
+		op.ballots = make(map[Ballot]bool)
+		op.r++
+		op.mu.Unlock()
+
+		// 4. send heartbeat to all peers
+		for peer := range op.peers {
+			if peer != op.me {
+				go func(r int, p int) {
+					request := HBRequest{Rnd: r}
+					reply := HBReply{}
+					ok := op.peers[p].Call("OmniPaxos.HeartBeatHandler", &request, &reply)
+					op.Trace("received heart beat from %d, round:%d, ballot:%d, q:%t", p, reply.Rnd, reply.Ballot, reply.Q)
+
+					if ok && reply.Rnd == r {
+						op.mu.Lock()
+						op.ballots[Ballot{N: reply.Ballot, Pid: p}] = reply.Q
+						op.mu.Unlock()
+					}
+				}(op.r, peer)
+			}
+		}
+
+		time.Sleep(delay)
+	}
+
+}
+
+func (op *OmniPaxos) updateMissedHbsAndReconnect() {
+	// Stub for A4
+	op.restart.loop++
+}
+
 func (op *OmniPaxos) persist() {
 	buf, _ := op.os.toBytes()
 	op.persister.SaveState(buf)
@@ -181,9 +252,13 @@ func Make(peers []*labrpc.ClientEnd, me int,
 	op.state = State{role: FOLLOWER, phase: PREPARE}
 	op.os = OmniPaxosState{L: Ballot{N: -1, Pid: -1}, Log: []interface{}{}, PromisedRnd: Ballot{N: -1, Pid: -1}, AcceptedRnd: Ballot{N: -1, Pid: -1}, DecidedIdx: 0}
 
+	// Init A4 fields
+	op.missedHbCounts = make(map[int]int)
+	op.restart = Restart{}
+
 	op.readPersist()
 
-	// go op.startTimer(op.delay) // Will be added in next commit
+	go op.startTimer(op.delay)
 
 	return op
 }
